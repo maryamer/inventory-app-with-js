d748ac89e5b890431b01f27ccdd536e032b9826b
maryameb <personalmaryamebrahimi@gmail.com>
2023-08-18 15:26:01 +0330

    categoryView.AddNewCategory

--- src/js/CategoryView.js
+++ src/js/CategoryView.js
@@ -1,11 +1,26 @@
 import Storage from "./Storage.js";
 
-// const categoryTitle = document.querySelector("#category-title");
-// const categoryDescription = document.querySelector("#category-description");
-// const addNewCetgoryBtn = document.querySelector("#add-new-category");
+const categoryTitle = document.querySelector("#category-title");
+const categoryDescription = document.querySelector("#category-description");
+const addNewCetgoryBtn = document.querySelector("#add-new-category");
 // const toggleAddCategoryBtn = document.getElementById("toggle-add-category");
 // const categoryWrapper = document.querySelector("#category-wrapper");
 // const cancelAddCategory = document.querySelector("#cancel-add-category");
-class CategoryView {}
+
+class CategoryView {
+  constructor() {
+    addNewCetgoryBtn.addEventListener("click", (e) => this.addNewCategory(e));
+    this.categories = [];
+  }
+  addNewCategory(e) {
+    e.preventDefault();
+    const title = categoryTitle.value;
+    const description = categoryDescription.value;
+    if (!title || !description) return;
+    Storage.saveCategory({ title, description });
+    this.categories = Storage.getAllCategories();
+    // update dom : update select option in category
+  }
+}
 
 export default new CategoryView();
